3549854bb74d39c197d5fe5331863e715528450f
Shmangles <jessieangles@gmail.com>
2010-10-22 14:29:15 -0700

    Initial commit to railsfordesigners.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsfordesigners::Application.config.secret_token = '68d2c06b1b680b9b71a8dd9706856019c5ad6b1e9f709468c1b1c27f1e90c7a9779e67e151dca9edfa38a948fd3d9f92eecd8c9ea80efb0355ea9fe980f0fba5'
